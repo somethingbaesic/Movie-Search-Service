5e738ed30da6af28052534709dad2eab832136df
Nathaphat Ongkunarak <lost.endin9@gmail.com>
2019-03-20 18:32:41 +0700

    Step 1 Download data from given source

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,29 +1,38 @@
 package com.wongnai.interview.movie.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestOperations;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
-	public static final String MOVIE_DATA_URL
-			= "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
+    public static final String MOVIE_DATA_URL
+            = "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
 
-	@Autowired
-	private RestOperations restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Override
+    public MoviesResponse fetchAll() {
+        //TODO:
+        // Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
+        // Please noted that you must only read data remotely and only from given source,
+        // do not download and use local file or put the file anywhere else.
 
-	@Override
-	public MoviesResponse fetchAll() {
-		//TODO:
-		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
-		// Please noted that you must only read data remotely and only from given source,
-		// do not download and use local file or put the file anywhere else.
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
+        messageConverters.add(converter);
+        restTemplate.setMessageConverters(messageConverters);
 
-		return null;
-	}
+        return restTemplate.getForObject(MOVIE_DATA_URL, MoviesResponse.class);
+    }
 }
