4d93591e422fba43461cdeb8e2cf515c5b880405
Nathaphat Ongkunarak <lost.endin9@gmail.com>
2019-03-21 23:05:28 +0700

    STEP 4 Search movies using inverted index and also delete unnecessary properties

--- src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/InvertedIndexMovieSearchService.java
@@ -1,6 +1,6 @@
 package com.wongnai.interview.movie.search;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
@@ -13,28 +13,75 @@ import com.wongnai.interview.movie.MovieSearchService;
 @Component("invertedIndexMovieSearchService")
 @DependsOn("movieDatabaseInitializer")
 public class InvertedIndexMovieSearchService implements MovieSearchService {
-	@Autowired
-	private MovieRepository movieRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
-	@Override
-	public List<Movie> search(String queryText) {
-		//TODO: Step 4 => Please implement in-memory inverted index to search movie by keyword.
-		// You must find a way to build inverted index before you do an actual search.
-		// Inverted index would looks like this:
-		// -------------------------------
-		// |  Term      | Movie Ids      |
-		// -------------------------------
-		// |  Star      |  5, 8, 1       |
-		// |  War       |  5, 2          |
-		// |  Trek      |  1, 8          |
-		// -------------------------------
-		// When you search with keyword "Star", you will know immediately, by looking at Term column, and see that
-		// there are 3 movie ids contains this word -- 1,5,8. Then, you can use these ids to find full movie object from repository.
-		// Another case is when you find with keyword "Star War", there are 2 terms, Star and War, then you lookup
-		// from inverted index for Star and for War so that you get movie ids 1,5,8 for Star and 2,5 for War. The result that
-		// you have to return can be union or intersection of those 2 sets of ids.
-		// By the way, in this assignment, you must use intersection so that it left for just movie id 5.
+    @Override
+    public List<Movie> search(String queryText) {
+        //TODO: Step 4 => Please implement in-memory inverted index to search movie by keyword.
+        // You must find a way to build inverted index before you do an actual search.
+        // Inverted index would looks like this:
+        // -------------------------------
+        // |  Term      | Movie Ids      |
+        // -------------------------------
+        // |  Star      |  5, 8, 1       |
+        // |  War       |  5, 2          |
+        // |  Trek      |  1, 8          |
+        // -------------------------------
+        // When you search with keyword "Star", you will know immediately, by looking at Term column, and see that
+        // there are 3 movie ids contains this word -- 1,5,8. Then, you can use these ids to find full movie object from repository.
+        // Another case is when you find with keyword "Star War", there are 2 terms, Star and War, then you lookup
+        // from inverted index for Star and for War so that you get movie ids 1,5,8 for Star and 2,5 for War. The result that
+        // you have to return can be union or intersection of those 2 sets of ids.
+        // By the way, in this assignment, you must use intersection so that it left for just movie id 5.
 
-		return null;
-	}
+        List<Movie> movieList = new ArrayList<>();
+        Map<String, Set<Long>> invertedIndexMovies = getInvertedIndex();
+        String[] wordFromQueryText = queryText.toLowerCase().split("\\s+");
+
+        List<Set<Long>> setList = new ArrayList<>();
+        boolean hasMatch = true;
+
+        for (String word : wordFromQueryText) {
+            if (invertedIndexMovies.containsKey(word)) {
+                setList.add(invertedIndexMovies.get(word));
+            }
+            else {
+                hasMatch = false;
+            }
+        }
+
+        if (hasMatch) {
+            Set<Long> movieId = setList.get(0);
+            for (Set<Long> set : setList) {
+                movieId.retainAll(set);
+            }
+
+            for (Long id : movieId) {
+                movieList.add(movieRepository.findById(id).get());
+            }
+
+        }
+        return movieList;
+    }
+
+    private Map<String, Set<Long>> getInvertedIndex() {
+
+        Map<String, Set<Long>> invertedIndex = new HashMap<>();
+        List<Movie> movieList = (List<Movie>) movieRepository.findAll();
+
+        for (Movie movie : movieList) {
+            String[] wordFromTitle = movie.getName().split("\\s+");
+            for (String word : wordFromTitle) {
+                word = word.toLowerCase().replaceAll("[^a-zA-Z]+", "");
+                Set<Long> movieIdSet = new HashSet<>();
+                if (invertedIndex.containsKey(word)) {
+                    movieIdSet = invertedIndex.get(word);
+                }
+                movieIdSet.add(movie.getId());
+                invertedIndex.put(word, movieIdSet);
+            }
+        }
+        return invertedIndex;
+    }
 }
