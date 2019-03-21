60e7078a56f97f9c87f148fe55cadc9ea9304bb6
Nathaphat Ongkunarak <lost.endin9@gmail.com>
2019-03-21 17:18:53 +0700

    STEP 3 Sync movie into repository and implement search movie in local database method

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,18 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MovieDataServiceImpl;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +26,13 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+
+		movieDataService = new MovieDataServiceImpl();
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		long generatedId = 0;
+		for(MovieData movieData: moviesResponse) {
+		    generatedId++;
+            movieRepository.save(new Movie(generatedId, movieData.getTitle(), movieData.getCast()));
+        }
 	}
 }
